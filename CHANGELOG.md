9f983c926f41a1888c34c5c7d4293fadf879d6bb
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-12-24 13:30:40 +0200

    0.0.2 release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.0.1
+* Initial release.
+* Show current temperature, by command run
+
+## 0.0.2
+* Round number to nearest to 0.5 decimal
+* Celsius/Fahrenheit designation depending on country
--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const request = require('request')
 
 const apiKey = 'f2d5b989dfafdab237be56401ef53185'
 
+const fahrenheitCountries = ['KY', 'BS', 'BZ', 'PW', 'US', 'PR', 'GU', 'UM']
+
 extIP.get().then(ip => {
   const geo = geoip.lookup(ip)
+  const isFahrenheitCountry = fahrenheitCountries.indexOf(geo.country) > -1
   const req = 'http://api.openweathermap.org/data/2.5/weather?'
   request(`${req}lat=${geo.ll[0]}&lon=${geo.ll[1]}&appid=${apiKey}&units=metric`,
     (err, res, body) => {
       if (!err) {
         const response = JSON.parse(body)
-        console.log(`Current temperature in ${response.name}: ${response.main.temp}`)
+        const temp = isFahrenheitCountry ? (response.main.temp * (9 / 5)) + 32 : response.main.temp
+        const tempRound = (Math.round(temp * 2) / 2).toFixed(1)
+        const tempScale = isFahrenheitCountry ? '°F' : '°C'
+        console.log(`Current weather in ${response.name}: ${tempRound}${tempScale}, ${response.weather[0].main}`)
       } else {
         console.log(`Can't get weather. Try again.\nMore: ${err}`)
       }
