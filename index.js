d4f006b8b8d2fdaed5dcf8d3eeecdfa23cfc7e18
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-05-02 00:46:13 +0300

    Packeges update, npmignore changed

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const request = require('request')
 
 const apiKey = 'f2d5b989dfafdab237be56401ef53185'
 
-const fahrenheitCountries = ['KY', 'BS', 'BZ', 'PW', 'US', 'PR', 'GU', 'UM']
+const imperialCountries = ['KY', 'BS', 'BZ', 'PW', 'US', 'PR', 'GU', 'UM']
 
 extIP.get().then(ip => {
   const geo = geoip.lookup(ip)
-  const isFahrenheitCountry = fahrenheitCountries.indexOf(geo.country) > -1
-  const req = 'http://api.openweathermap.org/data/2.5/weather?'
-  request(`${req}lat=${geo.ll[0]}&lon=${geo.ll[1]}&appid=${apiKey}&units=metric`,
+  const isImperialCountry = imperialCountries.indexOf(geo.country) > -1
+  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?'
+  const units = isImperialCountry ? 'imperial' : 'metric'
+  request(`${baseUrl}lat=${geo.ll[0]}&lon=${geo.ll[1]}&appid=${apiKey}&units=${units}`,
     (err, res, body) => {
       if (!err) {
         const response = JSON.parse(body)
-        const temp = isFahrenheitCountry ? (response.main.temp * (9 / 5)) + 32 : response.main.temp
-        const tempRound = (Math.round(temp * 2) / 2).toFixed(1)
-        const tempScale = isFahrenheitCountry ? '°F' : '°C'
+        const tempRound = (Math.round(response.main.temp * 2) / 2).toFixed(1)
+        const tempScale = isImperialCountry ? '°F' : '°C'
         console.log(`Current weather in ${response.name}: ${tempRound}${tempScale}, ${response.weather[0].main}`)
       } else {
         console.log(`Can't get weather. Try again.\nMore: ${err}`)
